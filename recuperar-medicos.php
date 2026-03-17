90be96d74e041b4c538d3ed337ad55fc97147227
LuizLins <luizmagao@gmail.com>
2026-03-17 20:28:26 -0300

    WIP: recuperar medicos

--- recuperar-medicos.php
+++ recuperar-medicos.php
@@ -1,12 +1,27 @@
 <?php
 
+use Luizlins\Projeto01\Modulos\Medico;
+
 require_once "vendor/autoload.php";
 
 $caminhoBanco = __DIR__ . "/banco.sqlite";
 $pdo = new PDO("sqlite:$caminhoBanco");
 
 $statement = $pdo->query("SELECT * FROM medicos;");
-foreach($statement->fetchAll() as $medico)
-{
-    echo $medico['nome'] . PHP_EOL;   
-}
+
+$listaMedicos = [];
+ 
+var_dump($statement->fetchAll());
+exit();
+
+// foreach($statement->fetchAll() as $medico)
+// {
+//     $listaMedicos[] = new Medico(
+//         $medico["id"],
+//         $medico["crm"],
+//         $medico["nome"],
+//         $medico["especialidade"]
+//     );
+// }
+
+var_dump($listaMedicos);
