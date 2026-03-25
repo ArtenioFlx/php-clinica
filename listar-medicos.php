f10e4e48b1b70c1c3d16461d41c6c765efa9b8f8
LuizLins <luizmagao@gmail.com>
2026-03-24 21:07:28 -0300

    WIP: executando sqlite de pacientes

--- listar-medicos.php
+++ listar-medicos.php
@@ -1,15 +1,10 @@
 <?php
 
-use Luizlins\Projeto01\Dominio\Modulos\Medico;
 use Luizlins\Projeto01\Infraestrutura\Repositorios\RepositorioMedico;
 
 require_once "vendor/autoload.php";
 
-$pdoMedico = new RepositorioMedico();
-$resposta = $pdoMedico->listar();
+$stmtMedico = new RepositorioMedico();
+$resposta = $stmtMedico->listar();
 
-$medico = new Medico(2, null, null, null);
-
-$pdoMedico->deletar($medico);
-
-var_dump($medico);
+var_dump($resposta);
